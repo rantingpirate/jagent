271e54bb36e5828f5e932ebc9e055cbd1812b9c1
James Wescott <james.wescott.5+gith@gmail.com>
2024-11-11 15:46:00 -0800

    Update to handle zombie agent directories

--- jagent.sh
+++ jagent.sh
@@ -2,25 +2,31 @@
 
 set pipefail
 
+ls=/bin/ls
 tmp=/tmp
 agentfile="$HOME/.ssh/$(hostname)-agent"
+running=0
 
-# function checkagent() {
-# 	ps -eo user,comm | grep -qe ssh-agent -e $USER
-# }
-#
-# function checkfile() {
-# 	[[ -f "$HOME/.$(hostname).agent" ]]
-# }
+# List all ssh-agent directories
+agentd=( $(find $tmp -maxdepth 1 -type d -name "ssh-*" -user $USER) )
 
-agentd=($(find $tmp -maxdepth 1 -type d -name "ssh-*" -user $USER))
-if ! [[ "$agentd" ]]; then
-	ssh-agent >$agentfile
-elif ! [[ -f $agentfile ]]; then
-	agents="$agentd/$(ls $agentd)"
+# Ignore zombie agent directories
+for dir in "${agentd[@]}"; do
+	agents="$dir/$( $ls "$dir" )"
 	pid=$(( ${agents##*.} + 1 ))
-	echo "SSH_AUTH_SOCK=/$agents; export SSH_AUTH_SOCK;" >$agentfile
-	echo "SSH_AGENT_PID=$pid; export SSH_AGENT_PID;" >>$agentfile
+
+	# If the agent is still running and is ours
+	if [[ -d /proc/$pid ]] && [[ $(stat -c '%U' /proc/$pid) == "$USER" ]]; then
+		echo "SSH_AUTH_SOCK=/$agents; export SSH_AUTH_SOCK;" >$agentfile
+		echo "SSH_AGENT_PID=$pid; export SSH_AGENT_PID;" >>$agentfile
+		running=1
+		break
+	fi
+done
+
+# If there wasn't a running agent, start one.
+if ! (( $running )); then
+	ssh-agent >$agentfile
 fi
 
 cat $agentfile
