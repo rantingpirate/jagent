53778c5eedfc5d53e60611f5d5171587167bb5f8
James Wescott <james.wescott.5+gith@gmail.com>
2024-11-11 17:11:49 -0800

    Remove unneeded file

--- jagent.sh
+++ jagent.sh
@@ -4,14 +4,10 @@ set pipefail
 
 ls=/bin/ls
 tmp=/tmp
-agentfile="$HOME/.ssh/$(hostname)-agent"
 running=0
 
 # List all ssh-agent directories
 agentd=( $(find $tmp -maxdepth 1 -type d -name "ssh-*" -user $USER) )
-
-# Make sure the agentfile is secure
-(umask 077 && touch "$agentfile")
 
 # Ignore zombie agent directories
 for dir in "${agentd[@]}"; do
@@ -21,8 +17,8 @@ for dir in "${agentd[@]}"; do
 	# If the agent is still running and is ours
 	if [[ -d /proc/$pid ]] && [[ $(stat -c '%U' /proc/$pid) == "$USER" ]]; then
 		# Fill the agentfile
-		echo "SSH_AUTH_SOCK=/$agents; export SSH_AUTH_SOCK;" >"$agentfile"
-		echo "SSH_AGENT_PID=$pid; export SSH_AGENT_PID;" >>"$agentfile"
+		echo "SSH_AUTH_SOCK=$agents; export SSH_AUTH_SOCK;"
+		echo "SSH_AGENT_PID=$pid; export SSH_AGENT_PID;"
 		running=1
 		break
 	fi
@@ -30,8 +26,10 @@ done
 
 # If there wasn't a running agent, start one.
 if ! (( $running )); then
-	ssh-agent >"$agentfile"
+	ssh-agent
 fi
 
-cat "$agentfile"
-
+# If not quiet, echo agent PID
+if ! [[ "-q" == "$1" ]]; then
+	echo "echo Agent pid $pid"
+fi
